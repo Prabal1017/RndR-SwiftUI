51fcad85a4627a657f05403cefbb84c2c18c2fa5
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-09-12 18:52:30 +0530

    Initial Commit

---
+++ RndR_SwiftUi/RndR_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RndR_SwiftUiApp.swift
+//  RndR_SwiftUi
+//
+//  Created by Piyush saini on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RndR_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
